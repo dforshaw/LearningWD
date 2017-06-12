dc186af29a30bf5a03d1620a2fe66c6173789be8
dforshaw <dfcv126@gmail.com>
2017-06-11 20:52:23 -0700

    updated baseURL

--- src/test/java/com/dfexamples/eviltester/Config.java
+++ src/test/java/com/dfexamples/eviltester/Config.java
@@ -2,7 +2,7 @@ package com.dfexamples.eviltester;
 
 public interface Config {
 
-    final String baseUrl = System.getProperty("baseUrl","http://www.waveville.net/~dforshaw/seleniumTestPages-ajr/pages");
+    final String baseUrl = System.getProperty("baseUrl","http://www.compendiumdev.co.uk/selenium");
 
     final String browser = System.getProperty("browser", "firefox");
 }
