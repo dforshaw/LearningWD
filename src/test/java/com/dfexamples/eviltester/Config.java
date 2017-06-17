5a3003814a7236ad943b4abb030af092c93d0b6f
dforshaw <dfcv126@gmail.com>
2017-06-17 00:58:30 -0700

    refactoring & then undoing it

--- src/test/java/com/dfexamples/eviltester/Config.java
+++ src/test/java/com/dfexamples/eviltester/Config.java
@@ -2,7 +2,7 @@ package com.dfexamples.eviltester;
 
 public interface Config {
 
-    final String baseUrl = System.getProperty("baseUrl","http://www.compendiumdev.co.uk/selenium");
+    final String baseUrl = System.getProperty("baseUrl","http://www.compendiumdev.co.uk/selenium/testpages");
 
-    final String browser = System.getProperty("browser", "firefox");
+    final String browser = System.getProperty("browser", "Firefox");
 }
