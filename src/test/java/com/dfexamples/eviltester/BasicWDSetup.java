456e9bb17f15dbd83a37c13efd4eb310e3748fee
David Forshaw <dfcv126@gmail.com>
2017-06-29 17:33:14 -0700

    initial refactor to have example use framework

--- src/test/java/com/dfexamples/eviltester/BasicWDSetup.java
+++ src/test/java/com/dfexamples/eviltester/BasicWDSetup.java
@@ -5,12 +5,19 @@ import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import static com.dfexamples.Framework.DriverManager.OperatingSystem;
+import static com.dfexamples.Framework.Enums.BrowserPaths.GECKO_DRIVER_PATH;
+import static com.dfexamples.Framework.Enums.BrowserPaths.WEBDRIVER_CLIENT_PATH;
+
 public class BasicWDSetup implements Config {
 
     protected static WebDriver driver;
 
     @Before
     public void setUp() {
+        String driverPath = WEBDRIVER_CLIENT_PATH.getPath() + GECKO_DRIVER_PATH.getPath();
+        driverPath =  OperatingSystem.equalsIgnoreCase("mac os x") ? driverPath : driverPath + ".exe";
+        System.setProperty("webdriver.gecko.driver", driverPath);
         driver = new FirefoxDriver();
     }
 
